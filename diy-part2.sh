b7ca164c7f5dd7a7e531662278bcacfc29659956
kwokpia <kwokpia@gmail.com>
2026-01-28 12:55:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 1. 自动定位内核配置文件 (24.10 通常使用 6.6 或更高级内核)
+KCONFIG=$(find target/linux/x86/config-* -maxdepth 0 | head -n 1)
+echo "正在注入 eBPF 特性到 24.10 内核配置: $KCONFIG"
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 2. 强制开启 Kprobes 和事件追踪
+sed -i '/CONFIG_KPROBES/d' "$KCONFIG"
+echo 'CONFIG_KPROBES=y' >> "$KCONFIG"
+echo 'CONFIG_KPROBE_EVENTS=y' >> "$KCONFIG"
+echo 'CONFIG_BPF_EVENTS=y' >> "$KCONFIG"
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 3. 开启 BTF (24.10 运行现代化 BPF 程序的基石)
+sed -i '/CONFIG_DEBUG_INFO_BTF/d' "$KCONFIG"
+echo 'CONFIG_DEBUG_INFO_BTF=y' >> "$KCONFIG"
+echo 'CONFIG_DEBUG_INFO_BTF_MODULES=y' >> "$KCONFIG"
+
+# 4. 开启 BPF 进阶支持
+echo 'CONFIG_BPF_SYSCALL=y' >> "$KCONFIG"
+echo 'CONFIG_BPF_JIT=y' >> "$KCONFIG"
+echo 'CONFIG_BPF_JIT_ALWAYS_ON=y' >> "$KCONFIG"
+echo 'CONFIG_HAVE_EBPF_JIT=y' >> "$KCONFIG"
+
+# 5. 开启 DWARF 调试信息（BTF 生成的先决条件）
+echo 'CONFIG_DEBUG_INFO=y' >> "$KCONFIG"
+echo 'CONFIG_DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT=y' >> "$KCONFIG"
+
+# 6. 扩容内核分区 (针对 J4125 建议扩至 64MB)
+sed -i 's/CONFIG_TARGET_KERNEL_PARTSIZE=16/CONFIG_TARGET_KERNEL_PARTSIZE=64/' .config
